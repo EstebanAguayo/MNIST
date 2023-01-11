53af4ec2b8593764050f3f054a5d6bd14b4458b4
EstebanAguayo <s23162@dtu.dk>
2023-01-11 10:52:17 +0100

    Initial Set Up

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='DTU 02427 ML Ops package',
+    author='Esteban',
+    license='',
+)
